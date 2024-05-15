d1377fc6d3909ac9e9ce503c12fca5ef240f7e01
purushotham yesu <114376679+Strangerpuru@users.noreply.github.com>
2024-05-15 22:30:30 +0530

    Add files via upload

---
+++ stringlength.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class stringlengthequal
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner (System.in);
+System.out.println("enter the strings");
+String n=sc.nextLine();
+String a=sc.nextLine();
+N=n.length();
+A=n.length();
+if(N.equals(A))
+{
+System.out.println("same");
+}
+else
+{
+System.out.println("not same");
+}
+}
+}
+
